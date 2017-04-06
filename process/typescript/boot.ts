9108cd2a92a2ae49c30765dfa52474e332412558
coder59 <joshikj.kec@gmail.com>
2017-04-06 23:45:59 +0530

    ho ja

---
+++ process/typescript/boot.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Welcome to File Transfer</h1>'
+})
+
+class AppComponent {
+
+}
+
+bootstrap(AppComponent);
